6fb7ba532d66f04ff22eaf6dbaa119611c73c829
juddus <github@juddus.com>
2021-04-05 16:34:13 +0100

    add breakpoints

---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,23 @@
+const breakMobileOnly = '@media (max-width: 640px)';
+const breakSmall = '@media (min-width: 640px)';
+const breakSmallOnly = '@media (min-width: 640px) and (max-width: 768px)';
+const breakMedium = '@media (min-width: 768px)';
+const breakMediumOnly = '@media (min-width: 768px) and (max-width: 1024px)';
+const breakLarge = '@media (min-width: 1024px)';
+const breakLargeOnly = '@media (min-width: 1024px) and (max-width: 1280px)';
+const breakXL = '@media (min-width: 1280px)';
+const breakXLOnly = '@media (min-width: 1280px) and (max-width: 1536px)';
+const break2XL = '@media (min-width: 1536px)';
+
+export {
+  breakMobileOnly,
+  breakSmall,
+  breakSmallOnly,
+  breakMedium,
+  breakMediumOnly,
+  breakLarge,
+  breakLargeOnly,
+  breakXL,
+  breakXLOnly,
+  break2XL,
+};
